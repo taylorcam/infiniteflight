db97227828ef17bf33dc862699423128cc35347a
Christopher Taylor <chris@jet-share.com>
2015-02-17 20:14:03 +0000

    no message

---
+++ Automation/Infinite Flight Airport Automation/InfiniteFlightAirport_Automation/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AirportParser
+{
+    class Program
+    {
+
+        static public void Main () {
+
+               /* FileStream filestream = new FileStream("C:\\Users\\Cameron\\Documents\\InfiniteFlight\\Airports\\out.txt", FileMode.Create);
+                var streamwriter = new StreamWriter(filestream);
+                streamwriter.AutoFlush = true;
+                Console.SetOut(streamwriter);
+                Console.SetError(streamwriter);
+*/
+                Console.WriteLine("Infinite Flight Airport Editing");
+                Console.WriteLine("© Copyright Cameron Carmichael Alonso, 2015. All rights reserved.\n");
+
+                //define functions
+                Console.WriteLine("Available functions:");
+                Console.WriteLine("1 - AptDivider");
+                Console.WriteLine("2 - ParkingSort");
+                Console.WriteLine("3 - BoundaryCreator");
+                Console.WriteLine("4 - BoundaryChecker");
+                Console.WriteLine("\nEnter the number for the function you wish to run.");
+
+
+            while (true) // Loop indefinitely 
+            {
+                //headers
+
+                string input = Console.ReadLine(); // input
+                if (input == "1")
+                {
+                    //selected AptDivider
+                    Console.WriteLine("Running AptDivider...\n");
+
+                    //check if file exists at base directory
+					if (File.Exists(AptDivider.MDat))
+                    {
+                        AptDivider.AptDividerFunction();
+
+                    } else {
+
+						Console.WriteLine("Error: Can't find directory at path: " + AptDivider.MDat);
+                        Console.WriteLine("Adjust BasePath variable to directory containing apt.mdat in root.");
+                        Console.WriteLine("Ending program...");
+                        Console.ReadKey();
+
+                    }
+                    
+
+                } else if (input == "2")
+                {
+                    //selected ParkingSort
+                    Console.WriteLine("Running ParkingSort...");
+
+                    ParkingSort.MainBlock();
+
+                }
+                else if (input == "3")
+                {
+                    //selected BoundaryCreator
+                    Console.WriteLine("Running BoundaryCreator...");
+                    BoundaryCreator.CreateBoundary();
+
+
+                }
+                else if (input == "4")
+                {
+                    //selected BoundaryChecker
+                    Console.WriteLine("Running BoundaryChecker...");
+                    CheckBoundaryDifference.CheckBoundary();
+
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Wrong value!");
+
+                }
+
+               
+
+            }
+
+        }
+        }
+    }
+
--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-Infinite Flight Airports
-==============
+<img src="https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/10978564_616559928490437_5560742316541826913_n.jpg?oh=75994dd96ae4fed3b0f634a2eace2fdc&oe=55602732&__gda__=1432543320_232e888febddbe1ad6b1b8fe7e55f273" />
 
-Check out https://github.com/carmichaelalonso/infiniteflight/blob/master/Getting_Started.md to get started.
+Welcome to Infinite Flight Airport Editing!
+===================
+Got a passion for design, and want to help improve Infinite Flight? Here, you can help contribute to airport designs, by adding taxiways, taxiway lines, parking spots and more. You'll be helping to pave the future for Infinite Flight airports!
+
+How to help
+===================
+Helping out is very easy once you understand the basics. We have full tutorials available on our website, as well as video tutorials:<br>
+**[carmichaelalonso.github.io/InfiniteFlight-Airports](http://carmichaelalonso.github.io/InfiniteFlight-Airports/)**<br><br>
+**Please check the [Issue Tracker](https://github.com/carmichaelalonso/InfiniteFlight-Airports/issues) before starting your airport. Make sure it isn't already in progress, and if it hasn't been started, please create a new issue with the name of the airport.**
+
+Do's and Don'ts
+==================
+**DO**<br>
+-Use accurate imagery for airports<br>
+-Add taxiways<br>
+-Add taxiway lines<br>
+-Add parking spots<br>
+-Modify runways<br>
+-Add airport boundaries<br>
+-Add airport ATC frequencies (please see websites such as airnav.com for this)<br>
+
+**DON'T**<br>
+-Add runway markings with taxiway lines (this includes runway numbers, blastpad arrows, etc.).<br>
+-Forget to include a boundary.<br>
+-Make up an airport - it **HAS** to be a real airport<br>
+
+One last thing...
+==================
+We appreciate every contribution that is made! Thank you very much to each person that contributes, you are really helping to make the Infinite Flight world a better place.
+
+**[Be sure to join our Facebook group, where you will get help on anything you need!](http://www.facebook.com/groups/infiniteflightairportediting/)**
+
+Airport editing run by Cameron Carmichael Alonso, Nik Barbour, Jonathan Perkins.
